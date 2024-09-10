5f7a27578c9a6f17cd64aff9da9bf3f57f100e78
Carol0021 <carolinedosreis437@gmail.com>
2024-09-10 17:57:02 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,474 @@
+export const games = [
+    {
+        name: "Super Mario",
+        description: "Super Mario All-Stars e Super Mario World",
+        image: "super-mario-world.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/embed/44617-super-mario-all-stars-super-mario-world-improvement.html"
+    },
+    {
+        name: "Super Bomberman 4",
+        description: "Jogo de ação e estratégia de 1996",
+        image: "bomberman4.jpeg",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/super-bomberman-4-english-translated.html"
+    },
+    {
+        name: "Contra 3",
+        description: "Contra 3 é um jogo clássico do Super Nintendo",
+        image: "Contra 3.jpg",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/contra-iii-the-alien-wars-restoration.html"
+    },
+    {
+        name: "Donkey Kong Country",
+        description: "Um jogo revolucionário de 1994",
+        image: "Donkey_Kong_Country_capa.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/donkey-kong-country-usa.html"
+    },
+    {
+        name: "Donkey Kong 2",
+        description: "Donkey Kong Country 2: Diddy's Kong Quest é um clássico absoluto do Super Nintendo, lançado em 1995",
+        image: "Donkey Kong 2.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/donkey-kong-country-2-diddy-s-kong-quest-usa-en-fr-rev-a.html"
+    },
+    {
+        name: "Donkey Kong 3",
+        description: "Donkey Kong Country 3 é a terceira parcela da popular série Donkey Kong Country",
+        image: "Donkey Kong 3.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/donkey-kong-country-3-dixie-kong-s-double-trouble-europe-en-fr-de.html"
+    },
+    {
+        name: "Final Fight",
+        description: "Final Fight 3 é um clássico dos jogos de luta",
+        image: "Final Fight 3.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/final-fight-3-europe.html"
+    },
+    {
+        name: "Mortal Kombat 2",
+        description: "Um dos jogos de luta mais icônicos e controversos da história dos videogames",
+        image: "MK-2.jpg",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/mortal-kombat-ii-usa.html"
+    },
+    {
+        name: "Muscle Bomber",
+        description: "É um jogo de luta livre profissional",
+        image: "Muscle bomber.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/muscle-bomber-the-body-explosion-japan.html"
+    },
+    {
+        name: "R-Type 3",
+        description: "É um jogo de tiro com rolagem lateral (shoot 'em up) lançado para o Super Nintendo em 1994",
+        image: "R-Type 3.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/r-type-iii-the-third-lightning-europe.html"
+    },
+    {
+        name: "Ronaldinho Soccer 97",
+        description: "É um jogo de SNES não oficial",
+        image: "Ronaldinho Soccer 97.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/superstar-soccer-2-ronaldinho-97.html"
+    },
+    {
+        name: "Star Fox 2",
+        description: "Star Fox 2 é um jogo de tiro espacial",
+        image: "Star Fox 2.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/star-fox-2-japan-proto2.html"
+    },
+    {
+        name: "Street Fighter 2 Turbo",
+        description: "Street Fighter II Turbo: Hyper Fighting ",
+        image: "Street Fighter 2.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/street-fighter-ii-turbo-hyper-fighting-usa.html"
+    },
+    {
+        name: "Sunset Riders",
+        description: "Sunset Riders: Uma Cavalgada Selvagem no Oeste Selvagem",
+        image: "Sunset riders.jpg",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/sunset-riders-europe.html"
+    },
+    {
+        name: "Ultimate MK3",
+        description: "Ultimate Mortal Kombat 3: A Evolução Definitiva da Franquia",
+        image: "Ultimate MK 3.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/genesis-games/ultimate-mortal-kombat-3-usa.html"
+    },
+    {
+        name: "Wolfenstein 3D",
+        description: "Wolfenstein 3D: Um marco nos jogos de tiro em primeira pessoa",
+        image: "Wolfenstein 3D.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/wolfenstein-3d-europe.html"
+    },
+    {
+        name: "X-MEN Mutant Apocalypse",
+        description: "X-Men: Mutant Apocalypse é um jogo de ação e plataforma lançado para o Super Nintendo em 1994",
+        image: "x-men.png",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/x-men-mutant-apocalypse-europe.html"
+    },
+     
+    {
+        name: "Marvel Super Heroes",
+        description: "War of the Gems",
+        image: "marvel.webp",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/snes-games/marvel-super-heroes-war-of-the-gems-usa.html"
+    },
+    {
+        name: "Sonic The Hedgehog (SNES)",
+        description: "Versão para Super Nintendo",
+        image: "sonic1.jpeg",
+        platform: "Nintendo",
+        gameLink: "https://www.retrogames.cc/mastersystem-games/sonic-genesis-for-master-system.html"
+    },
+    {
+        name: "Ben 10 Omniverse",
+        description: "Ben 10 Omniverse para Nintendo DS é um jogo emocionante",
+        image: "Nintendo DS/Ben 10 Omniverse.png",
+        platform: "Nintendo DS",
+        gameLink: "https://www.playemulator.io/nds-online/ben-10-omniverse/"
+    },
+    {
+        name: "Ben 10 Protector of Earth",
+        description: "Ben 10 Protector of Earth é um dos primeiros jogos da franquia",
+        image: "Nintendo DS/Ben 10 Protector of Earth.png",
+        platform: "Nintendo DS",
+        gameLink: "https://www.retrogames.cc/nds-games/ben-10-protector-of-earth-usa.html"
+    },
+    {
+        name: "Ben 10 Triple Pack",
+        description: "O Ben 10 Triple Pack reúne três jogos emocionantes do universo Ben 10",
+        image: "Nintendo DS/Ben 10 Triple Pack.png",
+        platform: "Nintendo DS",
+        gameLink: "https://www.playemulator.io/nds-online/ben-10-triple-pack/"
+    },
+    {
+        name: "Street Fighter V",
+        description: "Street Fighter V",
+        image: "Street Fighter V.png",
+        platform: "Nintendo",
+        gameLink: "https://www.playemulator.io/snes-online/street-fighter-5/"
+    },
+    {
+        name: "Diddy Kong Racing",
+        description: "É um jogo de corrida de kart lançado originalmente para o Nintendo 64 em 1997",
+        image: "Nintendo 64/Diddy Kong Racing.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.retrogames.cc/n64-games/diddy-kong-racing-usa-en-fr.html"
+    },
+    {
+        name: "Donkey Kong 64",
+        description: "Donkey Kong 64 é um jogo de plataforma 3D",
+        image: "Nintendo 64/Donkey Kong 64.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.retrogames.cc/n64-games/donkey-kong-64-usa.html"
+    },
+    {
+        name: "Mario Golf",
+        description: "Mario Golf é um jogo de esporte eletrônico",
+        image: "Nintendo 64/Mario Golf.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.playemulator.io/n64-online/mario-golf-n64/"
+    },
+    {
+        name: "Mario Kart",
+        description: "É um jogo de corrida",
+        image: "Nintendo 64/Mario Kart.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.retrogames.cc/n64-games/mario-kart-64-amped-up-v2-82.html"
+    },
+    {
+        name: "Mario Party 2",
+        description: "Mario Party 2 é um jogo clássico",
+        image: "Nintendo 64/Mario Party 2.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.playemulator.io/n64-online/mario-party-2/"
+    },
+    {
+name: "Mario Party 3",
+        description: "Mario Party 3 é um jogo divertido",
+        image: "Nintendo 64/Mario Party 3.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.playemulator.io/n64-online/mario-party-3/"
+    },
+    {
+        name: "Mario Party",
+        description: "Mario Party é uma série de jogos de festa muito popular ",
+        image: "Nintendo 64/Mario Party.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.retrogames.cc/n64-games/mario-party-64.html"
+    },
+    {
+        name: "Mario Tennis",
+        description: "Mario Tennis marcou uma nova era nos jogos eletrônico",
+        image: "Nintendo 64/Mario Tennis.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.playemulator.io/n64-online/mario-tennis-n64/"
+    },
+    {
+        name: "Mortal Kombat 4",
+        description: "Mortal Kombat 4 foi um dos jogos de luta mais populares da década de 90",
+        image: "Nintendo 64/Mortal Kombat 4.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.playemulator.io/n64-online/mortal-kombat-4-n64/"
+    },
+    {
+        name: "Star Fox 64",
+        description: "Star Fox 64 é um clássico jogo de tiro",
+        image: "Nintendo 64/Star Fox 64.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.retrogames.cc/n64-games/star-fox-64-usa.html"
+    },
+    {
+        name: "Super Mario 64",
+        description: "Super Mario 64 é um jogo de plataforma revolucionário lançado em 1996",
+        image: "Nintendo 64/Super Mario 64.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.playemulator.io/n64-online/super-mario-64/"
+    },
+    {
+        name: "Super Smash Bros",
+        description: "O Super Smash Bros",
+        image: "Nintendo 64/Super Smash Bros.png",
+        platform: "Nintendo 64",
+        gameLink: "https://www.playemulator.io/n64-online/super-smash-bros/"
+    },
+    {
+        name: "Bloddy Roar 1",
+        description: "Bloody Roar 1 foi um dos jogos de luta mais inovadores e sangrentos",
+        image: "PS1/Bloddy Roar 1.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/bloody-roar.html"
+    },
+    {
+        name: "Bloddy Roar 2",
+        description: "É um jogo bem popular",
+        image: "PS1/Bloddy Roar 2.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/bloody-roar-2-bringer-of-new-age.html"
+    },
+    {
+        name: "Capcom vs SNK Pro",
+        description: "Capcom vs. SNK Pro é um jogo de luta que reúne personagens icônicos das séries Street Fighter (Capcom) e The King of Fighters (SNK)",
+        image: "PS1/Capcom vs SNK Pro.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/capcom-vs-snk-millennium-fight-2000-pro-europe.html"
+    },
+    {
+        name: "Crash Bandicoot",
+        description: "Crash Bandicoot é uma franquia de jogos eletrônicos",
+        image: "PS1/Crash Bandicoot.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/crash-bandicoot.html"
+    },
+    {
+        name: "Dino Crisis 1",
+        description: "Dino Crisis 1 é um jogo de terror e sobrevivência lançado em 1999",
+        image: "PS1/Dino Crisis 1.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/dino-crisis.html"
+    },
+    {
+        name: "Dino Crisis 2",
+        description: "Dino Crisis 2 é um jogo de videogame do gênero survival horror",
+        image: "PS1/Dino Crisis 2.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/dino-crisis-ii.html"
+    },
+    {
+        name: "Driver",
+        description: "Driver é um jogo popular",
+        image: "PS1/Driver",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/driver-you-are-the-wheelman.html"
+    },
+    {
+        name: "Formula One Arcade",
+        description: "Formula One Arcade é um jogo de corrida",
+        image: "PS1/Formula One Arcade.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/formula-one-arcade.html"
+    },
+    {
+        name: "Gran Turismo 2",
+        description: "Gran Turismo 2 foi um marco na história dos jogos de corrida",
+        image: "PS1/Gran Turismo 2.png",
+        platform: "PlayStation",
+        gameLink: "https://www.playemulator.io/ps1-online/gran-turismo-2/"
+    },
+    {
+        name: "Marvel Super Heroes vs Street Fighter",
+        description: "O jogo Marvel Super Heroes vs. Street Fighter é um clássico dos jogos de luta",
+        image: "PS1/Marvel Super Heroes vs Street Fighter.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/marvel-super-heroes-vs-street-fighter.html"
+    },
+    {
+        name: "Marvel vs Capcom",
+        description: "Marvel vs. Capcom é uma série de jogos de luta",
+        image: "PS1/Marvel vs Capcom.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/marvel-vs-capcom-clash-of-super-heroes.html"
+    },
+    {
+        name: "Medal of Honor",
+        description: "Medal of Honor foi um marco na história dos jogos de tiro em primeira pessoa (FPS) ",
+        image: "PS1/Medal of Honor.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/medal-of-honor-usa.html"
+    },
+    {
+        name: "Mega Man 8",
+        description: "Mega Man 8 é um jogo eletrônico de plataforma da série Mega Man",
+        image: "PS1/Megaman 8.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/mega-man-8.html"
+    },
+    {
+        name: "Mega man Legends 1",
+        description: "Mega Man Legends foi um jogo de ação e aventura",
+        image: "PS1/Megaman Legends 1.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/mega-man-legends.html"
+    },
+    {
+        name: "Megaman Legends 2",
+        description: "Mega Man Legends 2 é um jogo de ação e aventura ",
+        image: "PS1/Megaman Legends 2.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/mega-man-legends-2.html"
+    },
+    {
+        name: "Metal Slug 1",
+        description: "Metal Slug 1 é um clássico dos jogos de tiro",
+        image: "PS1/Metal Slug 1.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/ngp-games/metal-slug-1st-mission-world-en-ja.html"
+    },
+    {
+        name: "Tekken 3",
+        description: "Embarque em uma épica batalha de artes marciais",
+        image: "PS1/Tekken 3.png",
+        platform: "PlayStation",
+        gameLink: "https://classicosps1.com.br/tekken-3/"
+    },
+    {
+        name: "Metal Slug X",
+        description: "Metal Slug X é um jogo de tiro em 2D de rolagem lateral",
+        image: "PS1/Metal Slug X.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/metal-slug-x.html"
+    },
+    {
+        name: "Mortal Kombat Trilogy",
+        description: "Mortal Kombat Trilogy é um jogo de luta que reúne o melhor dos três primeiros jogos",
+        image: "PS1/Mortal Kombat Trilogy.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/mortal-kombat-trilogy.html"
+    },
+    {
+        name: "Grand Theft Auto 2",
+        description: "Um dos clássicos do GTA",
+        image: "PS1/Grand Theft Auto 2.png",
+        platform: "PlayStation",
+        gameLink: "https://classicosps1.com.br/grand-theft-auto-2/"
+    },
+    {
+        name: "R-Type Delta",
+        description: "R-Type Delta é um jogo de tiro espacial ",
+        image: "PS1/R-Type Delta.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/r-type-delta-usa.html"
+    },
+    {
+        name: "Resident Evil 2",
+        description: "Resident Evil 2 é um jogo de terror de sobrevivência",
+        image: "PS1/Resident Evil 2.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/resident-evil-2-dual-shock-ver-disc-1-leon.html"
+    },
+    {
+        name: "Resident Evil 3 Nemesis",
+        description: "Resident Evil 3: Nemesis foi um marco nos jogos de terror",
+        image: "PS1/Resident Evil 3 Nemesis.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/resident-evil-3-nemesis-u.html"
+    },
+    {
+        name: "Homem Aranha 2",
+        description: "É um jogo bem legal de jogar",
+        image: "PS1/Homem Aranha 2.png",
+        platform: "PlayStation",
+        gameLink: "https://classicosps1.com.br/homem-aranha-2-enter-electro/"
+    },
+    {
+        name: "Silent Hill",
+        description: "Silent Hill é uma franquia de jogos eletrônicos",
+        image: "PS1/Silent Hill.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/silent-hill.html"
+    },
+    {
+        name: "Spec Ops Ranger Elite",
+        description: "Spec Ops: Ranger Elite é um jogo de tiro em terceira pessoa",
+        image: "PS1/Spec Ops Ranger Elite.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/spec-ops-ranger-elite.html"
+    },
+    {
+        name: "Street Fighter Collections 1",
+        description: "É uma coletânea de jogos da famosa série Street Fighter",
+        image: "PS1/Street Fighter Collections 1.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/street-fighter-collection-disc-1.html"
+    },
+    {
+        name: "Street Fighter Collections 2",
+        description: "Jogo bem famoso",
+        image: "PS1/Street Fighter Collections 2.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/street-fighter-collection-disc-2.html"
+    },
+    {
+        name: "Street Fighter Zero 3",
+        description: "Street Fighter Zero 3 é um jogo de luta que fez muito sucesso nos arcades",
+        image: "PS1/Street Fighter Zero 3.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/street-fighter-zero-3-japan.html"
+    },
+    {
+        name: "Spider Man",
+        description: "É um jogo popular",
+        image: "PS1/Homem Aranha.png",
+        platform: "PlayStation",
+        gameLink: "https://classicosps1.com.br/homem-aranha/"
+    },
+    {
+        name: "World's Scariest Police Chases",
+        description: "World's Scariest Police Chases é um jogo de videogame de ação e corrida",
+        image: "PS1/World's Scariest Police Chases.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/world-s-scariest-police-chases.html"
+    },
+    {
+        name: "X-Men vs Street Fighter",
+        description: "X-Men vs. Street Fighter foi um jogo de luta revolucionário",
+        image: "PS1/X-Men vs Street Fighter.png",
+        platform: "PlayStation",
+        gameLink: "https://www.retrogames.cc/psx-games/x-men-vs-street-fighter.html"
+    },
+]
+    
+    
